c06c475a112a42e43c07306098b5006846d28dc2
dianaguevarabs <141948935+dianaguevarabs@users.noreply.github.com>
2023-08-15 09:44:16 -0600

    Add files via upload

---
+++ generaciones computadoras.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main (int argc, char**argv)
+
+{
+	setlocale (LC_ALL,"");
+
+	printf("                     G E N E R A C I O N E S    D E    C O M P U T A D O R A S\n");
+	printf("\n");
+	printf("                                      PRIMERA GENERACIÓN\n");
+	printf("                                          1940-1956\n");
+	printf("                                 c a r a c t e r í s t i c a s\n");
+	printf("                   Utilización de tubos de vacío para amplificación y conmutación\n");
+	printf("                       Grandes, costosas y requerían sistemas de refrigeración\n");
+	printf("                    Aplicaciones principalmente en cálculos científicos y militares\n");
+	printf("                                       e j e m p l o s\n");
+	printf("                                         ENIAC,UNIVAC I\n");
+	printf("\n");
+	printf("                                      SEGUNDA GENERACIÓN\n");
+	printf("                                          1956-1963\n");
+	printf("                                 c a r a c t e r í s t i c a s\n");
+	printf("Uso de transistores en lugar de tubos de vacío, lo que redujo el tamaño y aumentO la confiabilidad\n");
+	printf("                     Mayor accesibilidad debido a una disminución en el costo\n");
+	printf("                            Aumento de la velocidad de procesamiento\n");
+	printf("                                       e j e m p l o s\n");
+	printf("                                      IBM 7090, CDC 1604\n");
+	printf("\n");
+	printf("                                      TERCERA GENERACIÓN\n");
+	printf("                                          1964-1971\n");
+	printf("                                 c a r a c t e r í s t i c a s\n");
+	printf("    Introducción de circuitos integrados (chips de silicio) para mayor integración de componentes.\n");
+	printf("                         Mejora en el rendimiento y reducción del tamaño.\n");
+	printf("          Surgimiento de sistemas operativos y lenguajes de programación más avanzados.\n");
+	printf("                                       e j e m p l o s\n");
+	printf("                                   IBM System/360, DEC PDP-8\n");
+	printf("\n");
+	printf("                                       CUARTA GENERACIÓN\n");
+	printf("                                           1971-1980\n");
+	printf("                                 c a r a c t e r í s t i c a s\n");
+	printf(" Desarrollo de microprocesadores, lo que permitió la creación de las computadoras personales (PC)\n");
+	printf("          Aumento drástico en la potencia de procesamiento y disminución de los costos\n");
+	printf("                    Mayor accesibilidad para individuos y pequeñas empresas\n");
+	printf("                                        e j e m p l o s\n");
+	printf("                              Apple II, IBM Personal Computer (PC)\n");
+	printf("\n");
+	printf("                                       QUINTA GENERACIÓN\n");
+	printf("                                        1980-en adelante\n");
+	printf("                                  c a r a c t e r í s t i c a s\n");
+	printf("         Continua mejora en la tecnología de microprocesadores, almacenamiento y conectividad\n");
+	printf("                   Surgimiento de dispositivos móviles, como smartphones y tablets\n");
+	printf("                       Revolución digital, con Internet como elemento central\n");
+	printf("                                        e j e m p l o s\n");
+	printf("         Computadoras personales modernas, dispositivos móviles, Internet, cloud computing\n");
+	
+	return 0;
+}
+
